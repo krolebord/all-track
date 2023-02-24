58f2e8e6cf771543899cac2b552df54b1dec5408
krolebord <krolebord@gmail.com>
2023-02-24 15:18:50 +0200

    Update currencies

--- packages/currencies/index.ts
+++ packages/currencies/index.ts
@@ -1,13 +1,59 @@
-import mappedCurrencies from "./currencies.json";
+import type validCurrencies from "./currencies";
 
 export type Currency = {
-  country: string;
-  name: string;
   symbol: string;
   code: number;
-  units: number;
+  sign: string;
+
+  name: string;
+  displayName: string;
+  
+  units: 2;
 };
 
-export const currencies: Currency[] = mappedCurrencies;
+type ValidCurrency = typeof validCurrencies[number];
+
+export const currencies = [
+  {
+    name: 'US Dollar',
+    displayName: 'US Dollar',
+    symbol: 'USD',
+    sign: '$',
+    code: 840,
+    units: 2,
+  },
+  {
+    name: 'Euro',
+    displayName: 'Euro',
+    symbol: 'EUR',
+    sign: '€',
+    code: 978,
+    units: 2,
+  },
+  {
+    name: 'Hryvnia',
+    displayName: 'Hryvnia',
+    symbol: 'UAH',
+    sign: '₴',
+    code: 980,
+    units: 2,
+  },
+  {
+    name: 'Zloty',
+    displayName: 'Zloty',
+    symbol: 'PLN',
+    sign: 'zł',
+    code: 985,
+    units: 2,
+  },
+  {
+    name: 'Pound Sterling',
+    displayName: 'Pound Sterling',
+    symbol: 'GBP',
+    sign: '£',
+    code: 826,
+    units: 2,
+  }
+] as const satisfies readonly (ValidCurrency & Omit<Currency, keyof ValidCurrency>)[];
 
 export const currencyCodes = currencies.map((currency) => currency.code);
